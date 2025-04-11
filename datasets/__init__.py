c6a4d8e58c905f54389f87896b751e624f2af0e3
jingyuSong <68323609+sjg918@users.noreply.github.com>
2025-04-11 21:08:37 +0900

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,5 @@
 from .kitti_dataset import KITTIDataset
 from .sceneflow_dataset import SceneFlowDatset
-from .drivingstereo_dataset import DrivingStereoDataset
 
 __datasets__ = {
     "sceneflow": SceneFlowDatset,
